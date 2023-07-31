f55eef31ca9b0169548ad84ca2e1b867550de5b2
sumit negi <nsumit181993@gmail.com>
2023-07-31 15:02:24 +0530

    h

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.use("/manpower", require("./ManPowerRoutes"));
-router.use("/manpower", require("./helpAndsupportRoutes"));
+router.use("/helpp", require("./helpAndsupportRoutes"));
 router.use("/employer",require("./employerRoutes"))
 router.use("/city",require("./selectcity"))
 router.use("/statee",require("./state"))
