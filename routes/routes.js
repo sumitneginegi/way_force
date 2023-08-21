bdeb10d281fd97a714960fb9da3ece6d8c9ee27b
sumit negi <nsumit181993@gmail.com>
2023-08-21 11:11:20 +0530

    d

--- routes/routes.js
+++ routes/routes.js
@@ -9,7 +9,7 @@ router.use("/city",require("./selectcity"))
 router.use("/statee",require("./state"))
 router.use("/category",require("./categoryROute"))
 router.use("/bookingByEmployerr",require("./bookingByEmployer"))
-router.use("/paymentt",require("./payment"))
+// router.use("/paymentt",require("./payment"))
 router.use("/OfferModell",require("./coupencode"))
 router.use("/termss",require("./admin/termsAndCondition"))
 
