f924581140d79c772363ef9b6b19ccdf917da92d
sumit negi <nsumit181993@gmail.com>
2023-08-22 14:22:16 +0530

    d

--- index.js
+++ index.js
@@ -10,48 +10,35 @@ const bodyparser = require("body-parser");
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var xhr = new XMLHttpRequest();
 
-
-
-
-// app.use(cors());
-const corsOptions = {
-  origin: ["http://localhost:3000", "http://way-force-nu.vercel.app"],
-  methods: ["GET", "POST"],
-  credentials: true,
-};
- 
-app.use(cors(corsOptions));
-
 app.use(express.json());
 app.use(bodyparser.urlencoded({ extended: true }));
-
-const PORT = process.env.PORT || 8080
 
 mongoose
   .connect(process.env.MONGO_DB_URL)
   .then(() => {
-    console.log("Db conneted succesfully");
+    console.log("Db connected successfully");
   })
   .catch((err) => {
     console.log(err);
   });
-app.get("/home",(req, res) => {
-  res.status(200).send({msg:"Working App"});
+
+// Set up your routes before applying CORS middleware
+app.get("/home", (req, res) => {
+  res.status(200).send({ msg: "Working App" });
 });
 
+app.use("/api/v1", require("./routes/routes"));
 
+// Configure CORS
+const corsOptions = {
+  origin: ["http://localhost:3000", "https://way-force-nu.vercel.app"],
+  methods: ["GET", "POST"],
+  credentials: true,
+};
+app.use(cors(corsOptions));
 
-app.use("/api/v1", require("./routes/routes"))
-
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-  console.log(`server listening on port ${PORT}`)
-})
-
-
-
-
-
-
-
-
+  console.log(`Server listening on port ${PORT}`);
+});
