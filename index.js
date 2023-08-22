9357c1edf9cf909c408f28ef7e3ce513d5c670fb
sumit negi <nsumit181993@gmail.com>
2023-08-22 10:46:01 +0530

    d

--- index.js
+++ index.js
@@ -1,26 +1,50 @@
 const express = require("express");
-const app = express();
-const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 const cors = require("cors");
-const dbConnect = require("./config/DBConnect");
-dotenv.config();
-dbConnect()
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const bodyparser = require("body-parser");
 
-app.use("/home", (req, res) => {
-    res.send("Hello World!");
+require("dotenv").config();
+
+
+// app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:3000", "http://way-force-nu.vercel.app"],
+  methods: ["GET", "POST"],
+  credentials: true,
+}));
+app.use(express.json());
+app.use(bodyparser.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 8080
+
+mongoose
+  .connect(process.env.MONGO_DB_URL)
+  .then(() => {
+    console.log("Db conneted succesfully");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+app.get("/home",(req, res) => {
+  res.status(200).send({msg:"Working App"});
 });
 
 
-app.use(express.json());
-// app.use(cors());
-app.use(cors({
-  origin: "http://localhost:3000",
-  methods: ["GET", "POST"],
-  credentials: true, // If needed
-}));
+
 app.use("/api/v1", require("./routes/routes"))
 
-const PORT = process.env.PORT || 8080;
+
 app.listen(PORT, () => {
   console.log(`server listening on port ${PORT}`)
 })
+
+
+
+
+
+
+
+
